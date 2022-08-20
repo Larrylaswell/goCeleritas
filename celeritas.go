8eb3d0cb0228d75883b19f578ed614b0c0fc957e
Larrylaswell <59291402+Larrylaswell@users.noreply.github.com>
2022-08-20 15:28:54 -0400

    added celeritas directory

---
+++ celeritas.go
@@ -0,0 +1,15 @@
+package celeritas
+
+func TestFunc(a, b int) int {
+	return a + b
+}
+
+func TestFunc2(a, b int) int {
+	return a - b
+}
+
+func TestFunc3(a, b int) int {
+	return a * b
+}
+
+// this is fun isnt it
